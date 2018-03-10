01857e83442a99b3f21e941a8e98ebbe2f22ceec
hikod <hikmetdogrul00@gmail.com>
2018-03-10 12:53:55 -0500

    added loop and print statement

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -4,6 +4,11 @@ public class GoodEvening {
 
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!!!");
+		for (int i = 0; i < 12; i++) {
+			System.out.println("it is still too early to go home");
+
+		}
+
 	}
 
 }
--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -4,6 +4,8 @@ public class GoodMorning {
 
 	public static void main(String[] args) {
 		System.out.println("Good morning Class!!!");
+		System.out.println("you guys need some coffee");
+		
 	}
 
 }
